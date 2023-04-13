d9bf8ea8afd78f29a95026e3999f249693c3d86a
Wouter van den Heuvel <exo_00@hotmail.com>
2023-04-13 17:57:13 -0300

    implemented buildLevel script

--- buildLevels.mjs
+++ buildLevels.mjs
@@ -4,17 +4,21 @@ import {fileURLToPath} from 'url'
 
 
 const __dirname = path.dirname( fileURLToPath(import.meta.url))
-const testFolder = `${__dirname}/public/xml`;
 
-const files = fs.readdirSync(testFolder)
+const files = fs.readdirSync(`${__dirname}/public/xml`)
 const levels = files.map((f, i) => ({
   name: `Level ${i + 1}`,
   path: `xml/${f}`
 }))
-console.log(JSON.stringify({ 
+const json = JSON.stringify({ 
   "_NOTE": "Dont write to this file. It will be overwritten when the app is compiled!",
   levels
-}))
-// const .forEach(file => {
-//   console.log(file);
-// });
+}, null, 2)
+
+try {
+  fs.writeFileSync(`${__dirname}/levels.json`, json);
+  // file written successfully
+  console.log("Succesfully written levels.json file!")
+} catch (err) {
+  console.error(err);
+}
