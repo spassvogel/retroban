b3c802846a2f3b7cf86966892668bb83b2652c94
Wouter van den Heuvel <exo_00@hotmail.com>
2023-04-30 23:50:10 -0300

    build to index.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import path from 'path';
-import fs from 'fs';
+import path from 'path'
+import fs from 'fs'
 
 const directoryPath = path.join(__dirname, 'public');
 fs.readdirSync(directoryPath)
 
-console.log('hiya')
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/retroban/',
   resolve: {
     alias: {
       stream: "stream-browserify", // needed for xml-js
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
     }
   },
   plugins: [
