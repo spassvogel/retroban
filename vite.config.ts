281a94ba9bc66679aae2a98c8debcaa3bd588a87
Wouter van den Heuvel <exo_00@hotmail.com>
2023-04-30 23:52:06 -0300

    removed unneeded stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import path from 'path'
-import fs from 'fs'
-
-const directoryPath = path.join(__dirname, 'public');
-fs.readdirSync(directoryPath)
 
 // https://vitejs.dev/config/
 export default defineConfig({
