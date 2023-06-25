6bf026142076de3f32c4aafd72ba83994d7c8d42
Wouter van den Heuvel <exo_00@hotmail.com>
2023-06-25 20:22:40 +0200

    display report table using available space

--- scripts/reportLevels.mjs
+++ scripts/reportLevels.mjs
@@ -1,40 +1,102 @@
 import fs from 'fs'
 import xmlJs from 'xml-js'
+
+import { Console } from 'console'
+import { Transform } from 'stream'
 import path from 'path'
 import {fileURLToPath} from 'url'
 import levelJson from '../src/levels.json' assert {
   type: 'json'
 }
 
+// possible args:
+// level <number> filter on level
+
+const args = parseArgs();
+
 // Reads the levels and presents the data
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const publicDir = path.resolve(__dirname, '../public')
 
-const table = levelJson.levels.map((l) => {
-  const contents = fs.readFileSync(`${publicDir}/${l.path}`)
-  const xml = xmlJs.xml2js(contents.toString(), { compact: true })
-  const {
-    name,
-    source,
-    sourceURL,
-    author,
-    authorEmail
-  } = xml.puzzle._attributes
+const columns = ['name', 'path', 'lvl', 'solution', 'sourceName', 'source', 'sourceURL', 'author', 'authorEmail']
+// used for calculating the available space
+const availableChars = process.stdout.columns - ((columns.length - 2) * "  |  ".length) - "│ (index) │".length - " │ 'xml/level0000.xml' │".length - "  |  ".length
 
-  let solution
-  if (xml.puzzle.solutions?.solution?._text) {
-    solution = xml.puzzle.solutions?.solution?._text
+const table = levelJson.levels
+  .filter((l) => {
+    return !args.level || +args.level === l.level
+  })
+  .map((l) => {
+    const {
+      name,
+      path,
+      level
+    } = l
+    const contents = fs.readFileSync(`${publicDir}/${l.path}`)
+    const xml = xmlJs.xml2js(contents.toString(), { compact: true })
+    const {
+      sourceName = name,
+      source,
+      sourceURL,
+      author,
+      authorEmail
+    } = xml.puzzle._attributes
+
+    let solution
+    if (xml.puzzle.solutions?.solution?._text) {
+      solution = xml.puzzle.solutions?.solution?._text
+    }
+
+    return {
+      name: name.substring(0, (1 / 7) * availableChars),
+      path,
+      lvl: level,
+      sourceName: sourceName?.substring(0, (1 / 7) * availableChars),
+      source: source?.substring(0, (1 / 7) * availableChars),
+      sourceURL: sourceURL?.substring(0, (1 / 7) * availableChars),
+      author: author?.substring(0, (1 / 7) * availableChars),
+      authorEmail: authorEmail?.substring(0, (1 / 7) * availableChars),
+      solution: solution?.substring(0, (1 / 7) * availableChars),
+    }
+  })
+
+console.table(table, columns)
+
+
+function printTable(input, columns) {
+  // @see https://stackoverflow.com/a/67859384
+  const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
+  const logger = new Console({ stdout: ts })
+  logger.table(input, columns)
+  const table = (ts.read() || '').toString()
+  let result = '';
+  for (let row of table.split(/[\r\n]+/)) {
+    let r = row.replace(/[^┬]*┬/, '┌');
+    r = r.replace(/^├─*┼/, '├');
+    r = r.replace(/│[^│]*/, '');
+    r = r.replace(/^└─*┴/, '└');
+    r = r.replace(/'/g, ' ');
+    result += `${r}\n`;
   }
+  console.log(result);
+}
 
-  return {
-    ...l,
-    sourceName: name,
-    source: source.substring(0, 45),
-    sourceURL: sourceURL?.substring(0, 22),
-    author: author?.substring(0, 24),
-    authorEmail,
-    solution: solution?.substring(0, 10)
+function parseArgs() {
+  const argMap = {}
+  const args = process.argv.slice(2);
+  if (args.length % 2)
+	throw new Error('Invalid number of arguments');
+
+  for (let i = 0; i < args.length; i += 2) {
+    let key = args[i];
+    if (!/^--([a-z]+-)*[a-z]+$/g.test(key))
+      throw new Error('Invalid argument name');
+      
+    key = key
+    .replace(/^--/, '')
+    .replace(/-([a-z])/g, g => g[1].toUpperCase());
+    
+    argMap[key] = args[i + 1];
   }
-})
-
-console.table(table, ['name', 'path', 'level', 'solution', 'sourceName', 'source', 'sourceURL', 'author', 'authorEmail'])
+  return argMap
+}
