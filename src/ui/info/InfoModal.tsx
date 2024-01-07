87c54c32c5276896cdc8b64d1687741af2c71e4f
Wouter van den Heuvel <exo_00@hotmail.com>
2024-01-07 12:39:31 +0100

    adjusted text

--- src/ui/info/InfoModal.tsx
+++ src/ui/info/InfoModal.tsx
@@ -26,7 +26,7 @@ const InfoModal = ({ onClose }: Props) => {
   if (!store) {
     return null
   }
-  
+
   return (
     <Modal onClose={onClose} bodyClassName="info-modal-body">
       <h2>Retroban</h2>
@@ -36,20 +36,21 @@ const InfoModal = ({ onClose }: Props) => {
           Sokoban is a classic puzzle game in which the player pushes boxes around a maze to specific locations. The rules are simple, but the game can become quite challenging.
           The goal of Sokoban is to push all the boxes onto designated target locations. Each target location must be covered by a box to complete a level.
         </p>
-
+        <p>
+          Use the arrow buttons or <BrowserView renderWithFragment>the keys on your keyboard</BrowserView><MobileView renderWithFragment>use swiping gestures</MobileView> to move.
+        </p>
         <div className="info-modal-body__demo-game-container">
           <Provider store={store}>
             <DemoGame />
           </Provider>
         </div>
-        <p>
-          Use the arrow buttons or <BrowserView renderWithFragment>the keys on your keyboard</BrowserView><MobileView renderWithFragment>use swiping gestures</MobileView> to move.
-        </p>
         <h3>Tips</h3>
         <p>
           Plan your moves in advance. Take a moment to observe the entire puzzle and plan your strategy. Avoid potential bottlenects where a box might get stuck against a wall or corner.
-          If you find yourself stuck or in a seemingly unwinnable situation, don't hesitate to use the 'undo' feature or restart the level. 
-          By planning your moves in advance, you'll develop a better understanding of the puzzle dynamics and improve your problem-solving skills in Sokoban. \
+          If you find yourself stuck or in a seemingly unwinnable situation, don't hesitate to use the 'undo' feature or restart the level. There is also the 'Solve' button, but use this as a last resort!
+        </p>
+        <p>
+          By planning your moves in advance, you'll develop a better understanding of the puzzle dynamics and improve your problem-solving skills in Sokoban.
           As you gain experience, you'll become more adept at quickly assessing puzzles and devising efficient solutions.
         </p>
         <h3>About this level</h3>
@@ -62,7 +63,7 @@ const InfoModal = ({ onClose }: Props) => {
         </div>
         { puzzleInfo.author && (
           <div>
-            By 
+            By
             <a href={`mailto:${puzzleInfo.authorEmail}`}>
               {` ${puzzleInfo.author}`}
             </a>
